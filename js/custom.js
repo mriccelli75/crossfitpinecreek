43416e1eb26d5683cc5aa5eb25a60ade762a81a0
Mike Riccelli <mriccelli@ons.org>
2019-01-16 14:28:48 -0500

    some js animation work

--- js/custom.js
+++ js/custom.js
@@ -34,10 +34,50 @@ $('.navbar .navbar-toggler').on('click', function () {
       }
   });
 
-   // Hide navbar when modals trigger
+// ------------------------------------------------------- //
+    // Facilities Modals
+// ------------------------------------------------------ //
+
+// Hide navbar when modals trigger
 $('.facility-modal').on('show.bs.modal', function(e) {
     $(".navbar").addClass("d-none");
   })
   $('.facility-modal').on('hidden.bs.modal', function(e) {
     $(".navbar").removeClass("d-none");
 });
+
+// ------------------------------------------------------- //
+    // Scroll Reveal Stuff
+    // ------------------------------------------------------ //
+
+    window.sr = ScrollReveal();
+    sr.reveal('#hero .intro-text', {
+        duration: 3000,
+        origin:'bottom'
+      });
+    
+    sr.reveal('section.about .order-md-1', {
+        duration: 2000,
+        origin:'left',
+        distance:'300px',
+        viewFactor: 0.2
+    });
+    
+    sr.reveal('section.about .order-md-2', {
+              duration: 2000,
+              origin:'right',
+              distance:'300px',
+              
+    });
+    sr.reveal('.img-container.img-left', {
+              duration: 2000,
+              origin:'left',
+              distance:'300px',
+              
+    });
+    sr.reveal('section#crossfit .col-md-6.text', {
+              duration: 2000,
+              origin:'right',
+              distance:'300px',
+              
+    });
